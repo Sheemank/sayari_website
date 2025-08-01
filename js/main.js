50998a3cd8d3354948cdc661c49df6399eb910be
Sheemank Rajvanshi <sheemankrajvanshi@gmail.com>
2025-08-01 11:17:29 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 {
                     id: 1,
                     title: "motivation student",
-                    imageUrl: 'https://github.com/Sheemank/sayari_website/blob/main/assets/images/motivation_1.jpg',
+                    imageUrl: 'https://plus.unsplash.com/premium_photo-1753211477530-ac7d65a3119f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8',
                     tags: ["motivation", "student", "heart", "study"],
                     description: "A beautiful expression of romantic love",
                     category: "motivation"               },
